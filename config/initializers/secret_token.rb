d140d15b5dbdea45c7f7286f851955b24920748f
Tom Scott <tomwscott@gmail.com>
2011-06-03 11:49:06 +0100

    Tom: Initial rails project for batch processing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Batch::Application.config.secret_token = '8283748ab3d281498d4230ba012d083ad5c20687bea0fb9071703b64d8f8ce428cb6b7ec8d4072f2e9d649f86ff77556a82408292c7c98be661380ac597cd0d5'
